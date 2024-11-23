369f8786d372b4e8700bdf6822919a7afdfcb70c
Dumasiks <63295716+Dumasiks@users.noreply.github.com>
2024-11-23 18:06:23 +0200

    adding Rule.Common

---
+++ Rule.Common/Extensions/InvalidIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rule.Common.Extensions
+{
+    public class InvalidIdException : CustomException
+    {
+        public InvalidIdException() { }
+        public InvalidIdException(string message) : base(message) { }
+        public InvalidIdException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
