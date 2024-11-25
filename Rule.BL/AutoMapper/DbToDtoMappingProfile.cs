4d3e8b375993dfa9708dfa9de22ea0c61a7c1235
Віталій Мельник <vitalii.melnyk@lnu.edu.ua>
2024-11-25 20:43:27 +0200

    Adding AutoMapper in Rule.BL

---
+++ Rule.BL/AutoMapper/DbToDtoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Rule.BL.Models;
+using Rule.DAL.Entities;
+
+namespace Rule.BL.AutoMapper
+{
+    public class DbToDtoMappingProfile: Profile
+    {
+        public DbToDtoMappingProfile()
+        {
+            CreateMap<Foundations, FoundationsDTO>().ReverseMap();
+            CreateMap<Posts, PostsDTO>().ReverseMap();
+            CreateMap<StatusPost, StatusPostDTO>().ReverseMap();
+            CreateMap<TypePost, TypePostDTO>().ReverseMap();
+            CreateMap<Users,  UsersDTO>().ReverseMap();
+        }
+    }
+}
